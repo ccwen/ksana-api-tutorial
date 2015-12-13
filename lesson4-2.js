9d775dd84c5542fae7d838e3cdd9b660c64b79a8
yapcheahshen <yapcheahshen@gmail.com>
2015-12-13 21:58:50 +0800

    remove api breadcrumb

--- lesson4-2.js
+++ lesson4-2.js
@@ -1,11 +1,11 @@
 console.log("lesson 4-2, breadcrumb");
 var ksa=require("ksana-simple-api");
 //given a uti or vpos, return breadcrumb in table-of-content
-
-ksa.breadcrumb({db:"cbeta",uti:"T13.0411.0768b",name:"mulu"},function(err,res){
-	console.log("breadcrumb,array of ancestor nodes",res.breadcrumb);
-	console.log("node seq",res.nodes);
+//,q:"菩提"
+ksa.toc({db:"cbeta",uti:"T13.0411.0768b",name:"mulu"},function(err,res){
+	console.log("breadcrumb,array of ancestor nodes\n",res.breadcrumb.map(function(r){return JSON.stringify(r)}).join("\n"));
+	console.log("node seq",res.nodeseq);
 
 	console.log("full toc length",res.toc.length);
-	console.log("res.breadcrumb[0]==res.toc[res.nodes[0]]");
+	console.log("res.breadcrumb[0]==res.toc[res.nodeseq[0]]");
 });
